1197580fada81ed888dc7bbd520b6646f6f95929
sirp140 <85513412+sirp140@users.noreply.github.com>
2023-05-19 06:46:02 +0000

    added model to display when user sign petition

---
+++ script.js
@@ -0,0 +1,8 @@
+let themeButton = document.getElementById("theme-button");
+let signNowButton = document.getElementById("sign-now-button");
+let count =3;
+const email = document.getElementById('emailInput');
+let revealableContainers = document.querySelectorAll(".revealable");
+let scaleFactor = 1;
+let modalImage = document.querySelector('.modal img');
+let closeModal = document.getElementById("closeModalBttn");
